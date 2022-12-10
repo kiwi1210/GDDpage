ce597f83e76fe73ce8533b67e3af960aa0e63ce1
kiwi1210 <80032842+kiwi1210@users.noreply.github.com>
2022-12-10 23:07:26 +0800

    Update script.js

--- asset/script.js
+++ asset/script.js
@@ -4,6 +4,8 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution: "&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"
 }).addTo(map);
 
+var layer = L.leafletGeotiff("data/all2015_235_210_Cn0_0.50.tif", bounds=[[25.0001, 121.3747], [25.1249, 121.50035]]).addTo(map);
+
 var ssp = document.getElementById("ssp").value;
 var year = document.getElementById("year").value;
 var gdd = document.getElementById("gdd").value;
