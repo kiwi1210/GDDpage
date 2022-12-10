3a7e2446242a67ad67c61bc92e866cb4ba872485
kiwi1210 <80032842+kiwi1210@users.noreply.github.com>
2022-12-10 23:34:52 +0800

    Update script.js

--- asset/script.js
+++ asset/script.js
@@ -1,8 +1,6 @@
-var map = L.map("map").setView([23.5, 121], 7);
-
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+var osm = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution: "&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"
-}).addTo(map);
+});
 
 var layer = L.leafletGeotiff("data/all2015_235_210_Cn0_0.50.tif", bounds=[[25.0001, 121.3747], [25.1249, 121.50035]]);
 
@@ -15,7 +13,8 @@ var options = {opacity: 1};
 var img = L.imageOverlay(imageUrl, imageBounds, options);
 
 var layers = L.layerGroup([layer, img]);
-layers.addTo(map);
+var map = L.map("map", {layers: [osm, layers]}).setView([23.5, 121], 7);
+
 
 document.getElementById("alpha").addEventListener("change", chgAlpha);
 document.getElementById("ssp").addEventListener("change", chgSsp);
