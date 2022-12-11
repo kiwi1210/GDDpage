a0f4b6bde7ce9018850d925e27744c50f159bf1f
kiwi1210 <80032842+kiwi1210@users.noreply.github.com>
2022-12-11 11:32:27 +0800

    Update script.js

--- asset/script.js
+++ asset/script.js
@@ -1,7 +1,16 @@
-var map = L.map("map").setView([23.5, 121], 7);
-
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+var osm = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution: "&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"
-}).addTo(map);
+});
 
-var layer = L.leafletGeotiff("data/all2015_235_210_Cn0_0.50.tif", bounds=[[25.0001, 121.3747], [25.1249, 121.50035]]).addTo(map);
+var layer = L.leafletGeotiff("data/all2015_235_210_Cn0_0.50.tif", bounds=[[25.0001, 121.3747], [25.1249, 121.50035]]);
+
+var map = L.map("map", {layers: [osm, layer]}).setView([23.5, 121], 7);
+
+var baseMaps = {
+    "OpenStreetMap": osm
+};
+
+var overlayMaps = {
+    "Layer": layer
+};
+var layerControl = L.control.layers(baseMaps, overlayMaps).addTo(map);
